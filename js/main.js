dce898bac60ca6db0a37859322bc2364f8ba22b5
innabatalova <batalovainna97@gmail.com>
2021-03-13 13:26:17 +0300

    Added double your-plan repo

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters
+
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+        
+
+ 
+});
+ 
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+
+  loop: true,
+
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport: false,
+  // },
+
+  //Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+        
+
+ 
+});
